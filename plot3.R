c33411687f6733dabfa57b07e8b13d686fb6e20e
Sergii Kavun <62832866+s-kav@users.noreply.github.com>
2020-04-17 19:16:34 +0300

    Add main files

---
+++ plot3.R
@@ -0,0 +1,53 @@
+library(data.table)
+
+Url <- "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
+## fixing a link to a variable
+download.file(Url, destfile = "./epc.zip", method = "curl")
+## downloading needed file
+
+unzip(zipfile = "./epc.zip", exdir = "./data")
+## unpack loaded file
+
+hpc <- data.table::fread("./data/household_power_consumption.txt",  header = TRUE, na.strings = "?", sep = ";", dec = ".", data.table = FALSE)
+## read a file to work dataframe correspodence to conditions: gap cells were indicated by sign "?"; separator between columns is ";"; with header
+
+object.size(hpc)
+## check a volume of memory for this dataframe
+##
+hpc$Date <- as.Date(hpc$Date, "%d/%m/%Y")
+## conversation the first column as Date by the appropriate format
+
+hpc_new <- subset(hpc, Date >= as.Date("2007-02-01") & Date <= as.Date("2007-02-02"))
+## cleaning our dataframe based on a range of dates
+
+rm(hpc)
+## deleting initial (generic) dataframe
+
+hpc_new <- hpc_new[complete.cases(hpc_new), ]
+## checking on losses values
+
+DaT <- paste(hpc_new$Date, hpc_new$Time, sep = " ")
+## form new vector from our work dataframe (from two columns)
+
+DaT <- strftime(DaT, format = "%d/%m/%Y %H:%M:%S")
+## reformatting new dataframe in accordance with needed Date & Time formats
+
+DaT <- as.POSIXct(DaT, tryFormats = "%d/%m/%Y %H:%M:%S")
+## conversation new vector
+
+hpc_new <- hpc_new[ ,!(names(hpc_new) %in% c("Date","Time"))]
+## deleting two excess cloumns from our work dataframe
+
+hpc_new <- cbind("Date_Time" = DaT, hpc_new)
+## obtaining new work dataframe with modified type of the first column and preparing to the following plots
+
+with(hpc_new, {
+        plot(Sub_metering_1 ~ Date_Time, type = "l",                              ylab = "Energy sub metering", xlab = "")
+        lines(Sub_metering_2 ~ Date_Time, col = "Red")
+        lines(Sub_metering_3 ~ Date_Time, col = "Blue")
+})
+legend("topright", col = c("black", "red", "blue"), lwd = c(1, 1, 1),
+       c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
+dev.copy(png,"./figure/plot3.png", width = 480, height = 480)
+dev.off()
+## plotting in accordance with needed conditions (Plot 3); write as PNG-file with specified resolutions (480 x 480 pxs); close a connection with a device
---
+++ plot4.R
@@ -0,0 +1,73 @@
+library(data.table)
+
+Url <- "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
+## fixing a link to a variable
+download.file(Url, destfile = "./epc.zip", method = "curl")
+## downloading needed file
+
+unzip(zipfile = "./epc.zip", exdir = "./data")
+## unpack loaded file
+
+hpc <- data.table::fread("./data/household_power_consumption.txt",  header = TRUE, na.strings = "?", sep = ";", dec = ".", data.table = FALSE)
+## read a file to work dataframe correspodence to conditions: gap cells were indicated by sign "?"; separator between columns is ";"; with header
+
+object.size(hpc)
+## check a volume of memory for this dataframe
+##
+hpc$Date <- as.Date(hpc$Date, "%d/%m/%Y")
+## conversation the first column as Date by the appropriate format
+
+hpc_new <- subset(hpc, Date >= as.Date("2007-02-01") & Date <= as.Date("2007-02-02"))
+## cleaning our dataframe based on a range of dates
+
+rm(hpc)
+## deleting initial (generic) dataframe
+
+hpc_new <- hpc_new[complete.cases(hpc_new), ]
+## checking on losses values
+
+DaT <- paste(hpc_new$Date, hpc_new$Time, sep = " ")
+## form new vector from our work dataframe (from two columns)
+
+DaT <- strftime(DaT, format = "%d/%m/%Y %H:%M:%S")
+## reformatting new dataframe in accordance with needed Date & Time formats
+
+DaT <- as.POSIXct(DaT, tryFormats = "%d/%m/%Y %H:%M:%S")
+## conversation new vector
+
+hpc_new <- hpc_new[ ,!(names(hpc_new) %in% c("Date","Time"))]
+## deleting two excess cloumns from our work dataframe
+
+hpc_new <- cbind("Date_Time" = DaT, hpc_new)
+## obtaining new work dataframe with modified type of the first column and preparing to the following plots
+
+
+hist(hpc_new$Global_active_power, main = "Global Active Power", xlab = "Global Active Power (kilowatts)", col = "red")
+dev.copy(png,"./figure/plot1.png", width = 480, height = 480)
+dev.off()
+## plotting a histogram in accordance with needed conditions (Plot 1); write as PNG-file with specified resolutions (480 x 480 pxs); close a connection with a device
+
+
+plot(hpc_new$Global_active_power ~ hpc_new$Date_Time, type = "l", ylab = "Global Active Power (kilowatts)", xlab = "")
+dev.copy(png,"./figure/plot2.png", width = 480, height = 480)
+dev.off()
+## plotting in accordance with needed conditions (Plot 2); write as PNG-file with specified resolutions (480 x 480 pxs); close a connection with a device
+
+par(mfrow = c(2, 2), mar = c(4, 4, 2, 1))
+## preparing our space for plotting
+
+with(hpc_new, {
+        plot(Global_active_power ~ Date_Time, type = "l",
+             ylab = "Global Active Power", xlab = "")
+        plot(Voltage ~ Date_Time, type = "l",
+             ylab = "Voltage", xlab = "datetime")
+        plot(Sub_metering_1 ~ Date_Time, type = "l",
+             ylab = "Energy sub metering", xlab = "")
+        lines(Sub_metering_2 ~ Date_Time, col = "Red")
+        lines(Sub_metering_3 ~ Date_Time, col = "Blue")
+        legend("topright", col = c("black", "red", "blue"), lty = 1, lwd = 2, bty = "n", legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
+        plot(Global_reactive_power ~ Date_Time, type = "l",
+             ylab = "Global_Rective_Power", xlab = "datetime")
+})
+dev.copy(png,"./figure/plot4.png", width = 480, height = 480)
+## plotting in accordance with needed conditions (Plot 4), 2 x 2 figures; write as PNG-file with specified resolutions (480 x 480 pxs); close a connection with a device
